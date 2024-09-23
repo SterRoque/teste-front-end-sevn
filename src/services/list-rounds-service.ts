5781b30a94417628766f60525c655904d7d94592
Sterphane Roque <stermiss2@gmail.com>
2024-09-23 17:43:33 -0300

    chore: add environments variables

--- src/services/list-rounds-service.ts
+++ src/services/list-rounds-service.ts
@@ -3,7 +3,7 @@ import { Round } from "../interfaces/tournament-interface";
 type TListRoundsServiceResponse = Round[];
 
 export async function listRoundsService(): Promise<TListRoundsServiceResponse> {
-  const response = await fetch("https://sevn-pleno-esportes.deno.dev/");
+  const response = await fetch(import.meta.env.VITE_APP_API_URL);
   const data = await response.json();
 
   return data;
