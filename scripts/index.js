c89743e05ea933851df5c7f2881cec283a96f53d
Sterphane Roque <stermiss2@gmail.com>
2024-09-23 16:18:55 -0300

    feat: rounds navigation

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,25 @@
 import { listRoundsService } from "./list-rounds-service.js";
 
-async function showRounds() {
-  const listRoundsServiceResponse = await listRoundsService();
+let currentRound = 1;
+let allRoundsGames = [];
 
-  const panelContainer = document.getElementById("panel");
+const gamesContainer = document.getElementById("games");
+const prevButton = document.getElementById("prev-button");
+const nextButton = document.getElementById("next-button");
+const currentRoundText = document.getElementById("current-round-text");
 
-  for (const game of listRoundsServiceResponse[0].games) {
+async function loadRoundsGames() {
+  allRoundsGames = await listRoundsService();
+}
+
+function showRounds() {
+  for (const game of allRoundsGames[currentRound - 1].games) {
     const teamHome = game.team_home_id.slice(-1);
     const teamAway = game.team_away_id.slice(-1);
 
     const lastGameOfCurrentRound =
-      listRoundsServiceResponse[0].games[
-        listRoundsServiceResponse[0].games.length - 1
+      allRoundsGames[currentRound - 1].games[
+        allRoundsGames[currentRound - 1].games.length - 1
       ];
 
     const isLastGameOfCurrentRound =
@@ -40,10 +48,49 @@ async function showRounds() {
 
  `;
 
-    panelContainer.innerHTML += htmlClassification;
+    gamesContainer.innerHTML += htmlClassification;
   }
 }
 
-onload = () => {
+function resetGamesContainer() {
+  gamesContainer.innerHTML = "";
+}
+
+function verifyNavigateButtons() {
+  if (currentRound === allRoundsGames.length) {
+    nextButton.disabled = true;
+  } else {
+    nextButton.disabled = false;
+  }
+
+  if (currentRound === 1) {
+    prevButton.disabled = true;
+  } else {
+    prevButton.disabled = false;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  await loadRoundsGames();
   showRounds();
-};
+
+  nextButton.addEventListener("click", () => {
+    currentRound++;
+    currentRoundText.textContent = `Rodada ${currentRound}`;
+
+    verifyNavigateButtons();
+
+    resetGamesContainer();
+    showRounds();
+  });
+
+  prevButton.addEventListener("click", () => {
+    currentRound--;
+    currentRoundText.textContent = `Rodada ${currentRound}`;
+
+    verifyNavigateButtons();
+
+    resetGamesContainer();
+    showRounds();
+  });
+});
